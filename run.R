db7baaedef2ae4859b7ed61f7c0519f43844493e
Louisa <louisahsmith@gmail.com>
2019-04-03 16:53:27 -0700

    include runApp

--- run.R
+++ run.R
@@ -9,8 +9,8 @@ if (length(args) == 0) {
 
 print(paste("Listening on port", port))
 
-# shiny::runApp(
-#   appDir = getwd(),
-#   host = "127.0.0.1",
-#   port = as.numeric(port)
-# )
+shiny::runApp(
+  appDir = getwd(),
+  host = "127.0.0.1",
+  port = as.numeric(port)
+)
