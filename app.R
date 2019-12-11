1721780ac6741415a79002b71e238697c2257b78
Louisa Smith <louisahsmith@gmail.com>
2019-12-11 15:24:38 -0500

    works aside from no error message when you uncheck necessary assumptions

--- app.R
+++ app.R
@@ -65,6 +65,8 @@ ui <- navbarPage(
   tabPanel(
     title = "Compute bound",
     mainPanel(
+      # this can go anywhere, just proving modal for assumptions
+      # TODO: add other modals??
       bs_modal(
         id = "modal_assumptions_B",
         title = "Additional assumptions",
@@ -79,75 +81,30 @@ ui <- navbarPage(
           "Risk difference" = "RD",
           "Odds ratio" = "OR"
         )
-      ),
+      ), 
+      
+      # if they choose OR there's only one option
       conditionalPanel(
         condition = "input.outcomeType_B == 'OR'",
-        p("Bound only available for odds ratio when bias is due to control selection in a case-control study. For other types of selection bias, choose risk ratio or difference."),
+        p("Bound only available for odds ratio when bias is due to control selection in a case-control study. 
+          For other types of selection bias, choose risk ratio or difference."),
         checkboxGroupInput(
           "control_sel_assump",
-          label = HTML('<label class="control-label">Additional assumptions</label>
+          label = HTML('<label class="control-label">Necessary assumptions</label>
         <a href="#" data-toggle="modal" data-target="#modal_control_sel_assump">
              <i class="fa fa-info-circle"></i>
              </a>'),
           choices = c(
-            "Selection of cases (but not controls) independent of exposure" = "non_indep"
-          )
-        ),
-      ),
-      conditionalPanel(
-        condition = "input.outcomeType_B == 'RR' | input.outcomeType_B == 'RD'",
-        checkboxGroupInput(
-          "assump_B",
-          label = HTML('<label class="control-label">Additional assumptions</label>
-        <a href="#" data-toggle="modal" data-target="#modal_assumptions_B">
-             <i class="fa fa-info-circle"></i>
-             </a>'),
-          choices = c(
-            "Unmeasured factor a defining characteristic of selection" = "S_eq_U",
-            "Selection always associated with increased risk of outcome in both exposure groups" = "risk_inc",
-            "Selection always associated with decreased risk of outcome in both exposure groups" = "risk_dec",
-            "Inference only in selected population" = "sel_pop"
-          )
-        )),
-      # extra inputs for risk difference
-      conditionalPanel(
-        condition = "input.outcomeType_B == 'RD'",
-        splitLayout(
-          numericInput("pY_S1_A1", HTML("Risk in selected exposed:<br>$P(Y = 1 \\mid A = 1, S = 1)$"),
-                       value = NA, min = 0, max = 1, step = 0.1
+            "No unmeasured confounding \\({(Y^a \\amalg A \\mid C)}\\)" = "no_confound",
+            "Selection of cases independent of exposure \\({(S \\amalg A \\mid Y = 1, C )}\\)" = "case_indep",
+            "Selection of controls independent of exposure, conditional on some possibly unmeasured factor(s) 
+            \\(U\\) 
+            \\({(S \\amalg A \\mid  Y = 0, U, C)}\\)" = "control_indep"
           ),
-          numericInput("pY_S1_A0", HTML("Risk in selected unexposed:<br>$P(Y = 1 \\mid A = 0, S = 1)$"),
-                       value = NA, min = 0, max = 1, step = 0.1
-          )
-        )),
-      
-      # conditional panels for all of the various possible assumptions
-      HTML("<label class='control-label'>Estimated/hypothesized values for parameters</label>"),
-      conditionalPanel(
-        condition = "output.sel_pop && !output.S_eq_U && !output.risk_inc && !output.risk_dec",
-        splitLayout(
-          numericInput("RRUYS1", "$\\text{RR}_{UY|S=1}$", NA) %>%
-            shinyInput_label_embed(
-              shiny_iconlink() %>%
-                bs_embed_popover(
-                  title = NA,
-                  content = "Maximum outcome risk ratio comparing two levels of U among the selected",
-                  placement = "top"
-                )
-            ),
-          numericInput("RRAUS1", "$\\text{RR}_{AU|S=1}$", NA) %>%
-            shinyInput_label_embed(
-              shiny_iconlink() %>%
-                bs_embed_popover(
-                  title = NA,
-                  content = "Maximum factor by which exposure increases some value of U among the selected",
-                  placement = "top"
-                )
-            )
-        )
-      ),
-      conditionalPanel( # TODO: the other ones need to disappear when this happens
-        condition = "output.non_indep",
+          selected = c("case_indep", "control_indep", "no_confound")
+        ),
+        HTML("<label class='control-label'>Estimated/hypothesized values for parameters</label>"),
+        
         splitLayout(
           # TODO: Fix content in popovers
           numericInput("RRUA1", "$\\text{RR}_{UA_1}$", NA) %>%
@@ -188,8 +145,123 @@ ui <- navbarPage(
             )
         )
       ), # end conditional panel for control selection
+      
+      # if not OR, what is the target of inference
       conditionalPanel(
-        condition = "!output.sel_pop && !output.risk_inc && !output.risk_dec && !output.S_eq_U",
+        condition = "input.outcomeType_B != 'OR'",
+        radioButtons(
+          "pop_group",
+          label = HTML('<label class="control-label">Target population</label>
+        <a href="#" data-toggle="modal" data-target="#modal_pop_group">
+             <i class="fa fa-info-circle"></i>
+             </a>'),
+          choices = c(
+            "Entire population" = "whole_pop",
+            "Selected population" = "sel_pop"
+          ),
+          selected = "whole_pop"
+        )
+      ),
+      
+      conditionalPanel( # "Inference only in selected population"
+        condition = "input.outcomeType_B != 'OR' && input.pop_group == 'sel_pop'",
+        
+        checkboxGroupInput( # TODO: needs modal?
+          "sel_pop_assump",
+          label = HTML('<label class="control-label">Necessary assumptions</label>
+        <a href="#" data-toggle="modal" data-target="#modal_sel_pop_assump">
+             <i class="fa fa-info-circle"></i>
+             </a>'),
+          choices = c(
+            "No unmeasured confounding \\({(Y_a \\amalg A \\mid C)}\\)" = "no_confound",
+            "All selection bias is captured by possibly unmeasured factor(s) \\(U\\) \\({(Y_a \\amalg A \\mid S = 1, U, C)}\\)" = 
+            "U_indep"
+          ),
+          selected = c("U_indep", "no_confound")
+        ),
+        
+        HTML("<label class='control-label'>Estimated/hypothesized values for parameters</label>"),
+        splitLayout(
+          numericInput("RRUYS1", "$\\text{RR}_{UY|S=1}$", NA) %>%
+            shinyInput_label_embed(
+              shiny_iconlink() %>%
+                bs_embed_popover(
+                  title = NA,
+                  content = "Maximum outcome risk ratio comparing two levels of U among the selected",
+                  placement = "top"
+                )
+            ),
+          numericInput("RRAUS1", "$\\text{RR}_{AU|S=1}$", NA) %>%
+            shinyInput_label_embed(
+              shiny_iconlink() %>%
+                bs_embed_popover(
+                  title = NA,
+                  content = "Maximum factor by which exposure increases some value of U among the selected",
+                  placement = "top"
+                )
+            )
+        ),
+        # extra inputs for risk difference
+        conditionalPanel(
+          condition = "input.pop_group == 'sel_pop' && input.outcomeType_B == 'RD'",
+          splitLayout(
+            numericInput("pY_S1_A1_sel", HTML("Risk in selected exposed:<br>$P(Y = 1 \\mid A = 1, S = 1)$"),
+                         value = NA, min = 0, max = 1, step = 0.1
+            ),
+            numericInput("pY_S1_A0_sel", HTML("Risk in selected unexposed:<br>$P(Y = 1 \\mid A = 0, S = 1)$"),
+                         value = NA, min = 0, max = 1, step = 0.1
+            )
+          ))
+      ), # end selected population option
+      
+      conditionalPanel(
+        condition = "input.outcomeType_B != 'OR' && input.pop_group != 'sel_pop'",
+        # TODO: does this need modal?
+        checkboxGroupInput(
+          "whole_pop_assump",
+          label = HTML('<label class="control-label">Necessary assumptions</label>
+        <a href="#" data-toggle="modal" data-target="#modal_whole_pop_assump">
+             <i class="fa fa-info-circle"></i>
+             </a>'),
+          choices = c(
+            "No unmeasured confounding \\({(Y_a \\amalg A \\mid C)}\\)" = "no_confound",
+            "Selection is only related to outcome via unmeasured factor(s) \\(U\\) \\({(Y \\amalg S \\mid A, U, C)}\\)" = 
+              "U_indep"
+          ),
+          selected = c("U_indep", "no_confound")
+        ),
+        
+        checkboxGroupInput(
+          "assump_B",
+          label = HTML('<label class="control-label">Additional assumptions</label>
+        <a href="#" data-toggle="modal" data-target="#modal_assumptions_B">
+             <i class="fa fa-info-circle"></i>
+             </a>'),
+          choices = c(
+            "Unmeasured factor a defining characteristic of selection" = "S_eq_U",
+            "Selection always associated with increased risk of outcome in both exposure groups" = "risk_inc",
+            "Selection always associated with decreased risk of outcome in both exposure groups" = "risk_dec"
+          )
+        ),
+      
+      HTML("<label class='control-label'>Estimated/hypothesized values for parameters</label>"),
+      
+      # extra inputs for risk difference
+      conditionalPanel(
+        condition = "input.pop_group != 'sel_pop' && input.outcomeType_B == 'RD'",
+        splitLayout(
+          numericInput("pY_S1_A1_whole", HTML("Risk in selected exposed:<br>$P(Y = 1 \\mid A = 1, S = 1)$"),
+                       value = NA, min = 0, max = 1, step = 0.1
+          ),
+          numericInput("pY_S1_A0_whole", HTML("Risk in selected unexposed:<br>$P(Y = 1 \\mid A = 0, S = 1)$"),
+                       value = NA, min = 0, max = 1, step = 0.1
+          )
+        )),
+      
+      # conditional panels for all of the various possible assumptions
+      
+      conditionalPanel(
+        condition = "input.outcomeType_B != 'OR' && input.pop_group != 'sel_pop' && !output.risk_inc && !output.risk_dec && !output.S_eq_U",
         splitLayout(
           RRUY0(1),
           RRUY1(1),
@@ -198,34 +270,34 @@ ui <- navbarPage(
         )
       ),
       conditionalPanel(
-        condition = "output.S_eq_U && !output.risk_inc && !output.risk_dec && !output.sel_pop",
+        condition = "input.outcomeType_B != 'OR' && input.pop_group != 'sel_pop' && output.S_eq_U && !output.risk_inc && !output.risk_dec",
         splitLayout(
           RRUY0(2),
           RRUY1(2)
         )
       ),
       conditionalPanel(
-        condition = "output.S_eq_U && output.risk_inc && !output.sel_pop && !output.risk_dec",
+        condition = "input.outcomeType_B != 'OR' && input.pop_group != 'sel_pop' && output.S_eq_U && output.risk_inc && !output.risk_dec",
         RRUY1(3)
       ),
       conditionalPanel(
-        condition = "output.S_eq_U && output.risk_dec && !output.sel_pop && !output.risk_inc",
+        condition = "input.outcomeType_B != 'OR' && input.pop_group != 'sel_pop' && output.S_eq_U && output.risk_dec && !output.risk_inc",
         RRUY0(3)
       ),
       conditionalPanel(
-        condition = "output.risk_inc && !output.S_eq_U && !output.sel_pop && !output.risk_dec",
+        condition = "input.outcomeType_B != 'OR' && input.pop_group != 'sel_pop' && output.risk_inc && !output.S_eq_U && !output.risk_dec",
         splitLayout(
           RRUY1(4),
           RRSU1(4)
         )
       ),
       conditionalPanel(
-        condition = "output.risk_dec && !output.S_eq_U && !output.sel_pop && !output.risk_inc",
+        condition = "input.outcomeType_B != 'OR' && input.pop_group != 'sel_pop' && output.risk_dec && !output.S_eq_U && !output.risk_inc",
         splitLayout(
           RRUY0(4),
           RRSU0(4)
         )
-      ),
+      )),
       
       # display results
       wellPanel(span(uiOutput("result.text_B"))),
@@ -332,11 +404,14 @@ server <- function(input, output, session) {
   
   #### compute bound tab -----------------------------------------------
   # need these to choose conditionally which parameter boxes to show
-  output$non_indep <- reactive({
-    "non_indep" %in% input$control_sel_assump
+  output$control_assump <- reactive({
+    all(c("case_indep", "control_indep", "no_confound") %in% input$control_sel_assump)
   })
-  output$sel_pop <- reactive({
-    "sel_pop" %in% input$assump_B
+  output$sel_pop_assump <- reactive({
+    all(c("U_indep", "no_confound") %in% input$sel_pop_assump) 
+  })
+  output$whole_pop_assump <- reactive({
+    all(c("U_indep", "no_confound") %in% input$whole_pop_assump) 
   })
   output$S_eq_U <- reactive({
     "S_eq_U" %in% input$assump_B
@@ -348,27 +423,26 @@ server <- function(input, output, session) {
     "risk_dec" %in% input$assump_B
   })
   
-  outputOptions(output, "non_indep", suspendWhenHidden = FALSE)
-  outputOptions(output, "sel_pop", suspendWhenHidden = FALSE)
+  outputOptions(output, "control_assump", suspendWhenHidden = FALSE)
+  outputOptions(output, "sel_pop_assump", suspendWhenHidden = FALSE)
+  outputOptions(output, "whole_pop_assump", suspendWhenHidden = FALSE)
   outputOptions(output, "S_eq_U", suspendWhenHidden = FALSE)
   outputOptions(output, "risk_inc", suspendWhenHidden = FALSE)
   outputOptions(output, "risk_dec", suspendWhenHidden = FALSE)
-  
+
   # compute bound computation
   bounds <- reactive({
-    non_indep <- "non_indep" %in% input$non_indep
-    sel_pop <- "sel_pop" %in% input$assump_B
+    control_assump <- all(c("case_indep", "control_indep", "no_confound") %in% input$control_sel_assump)
+    sel_pop_assump <- all(c("U_indep", "no_confound") %in% input$sel_pop_assump)
+    whole_pop_assump <- all(c("U_indep", "no_confound") %in% input$whole_pop_assump)
     S_eq_U <- "S_eq_U" %in% input$assump_B
     risk_inc <- "risk_inc" %in% input$assump_B
     risk_dec <- "risk_dec" %in% input$assump_B
-    
     validate(
       need(
-        !all(c("risk_inc", "risk_dec") %in% input$assump_B) &
-          !all(c("sel_pop", "risk_inc") %in% input$assump_B) &
-          !all(c("sel_pop", "risk_dec") %in% input$assump_B) &
-          !all(c("sel_pop", "S_eq_U") %in% input$assump_B),
-        "You have unnecessary and/or incompatible assumptions"
+        !(all(c("risk_inc", "risk_dec") %in% input$assump_B) & whole_pop_assump) |
+          sel_pop_assump | control_assump,
+        "You have missing or incompatible assumptions"
       )
     )
     
@@ -388,7 +462,7 @@ server <- function(input, output, session) {
       return(BF(input$RRUA1, input$RRS0U) * BF(input$RRUA0, input$RRS1U))
     }
     if (input$outcomeType_B == "RR") {
-      if (sel_pop) {
+      if (input$pop_group == "sel_pop") {
         return(BF(input$RRAUS1, input$RRUYS1))
       }
       if (!S_eq_U & !risk_inc & !risk_dec) {
@@ -411,37 +485,37 @@ server <- function(input, output, session) {
       }
     }
     if (input$outcomeType_B == "RD") {
-      if (sel_pop) {
+      if (input$pop_group == "sel_pop") {
         bf <- BF(input$RRAUS1, input$RRUYS1)
         return(max(c(
-          input$pY_S1_A0 * bf,
-          input$pY_S1_A1 * (1 - 1 / bf)
+          input$pY_S1_A0_sel * bf,
+          input$pY_S1_A1_sel * (1 - 1 / bf)
         )))
       }
       if (!S_eq_U & !risk_inc & !risk_dec) {
         bf0 <- BF(input$RRUY01, input$RRSU01)
         bf1 <- BF(input$RRUY11, input$RRSU11)
-        return(bf1 - input$pY_S1_A1 / bf1 + input$pY_S1_A0 * bf0)
+        return(bf1 - input$pY_S1_A1_whole / bf1 + input$pY_S1_A0_whole * bf0)
       }
       if (S_eq_U & !risk_inc & !risk_dec) {
         return(input$RRUY12 -
-                 input$pY_S1_A1 / input$RRUY12 +
-                 input$pY_S1_A0 * input$RRUY02)
+                 input$pY_S1_A1_whole / input$RRUY12 +
+                 input$pY_S1_A0_whole * input$RRUY02)
       }
       if (S_eq_U & risk_inc) {
-        return(input$RRUY13 - input$pY_S1_A1 / input$RRUY13)
+        return(input$RRUY13 - input$pY_S1_A1_whole / input$RRUY13)
       }
       # CHECK THESE
       if (S_eq_U & risk_dec) {
-        return(input$RRUY03 - input$pY_S1_A0 / input$RRUY03)
+        return(input$RRUY03 - input$pY_S1_A0_whole / input$RRUY03)
       }
       if (risk_inc) {
         bf <- BF(input$RRUY14, input$RRSU14)
-        return(bf - input$pY_S1_A1 / bf)
+        return(bf - input$pY_S1_A1_whole / bf)
       }
       if (risk_dec) {
         bf <- BF(input$RRUY04, input$RRSU04)
-        return(bf - input$pY_S1_A0 / bf)
+        return(bf - input$pY_S1_A0_whole / bf)
       }
     }
   })
